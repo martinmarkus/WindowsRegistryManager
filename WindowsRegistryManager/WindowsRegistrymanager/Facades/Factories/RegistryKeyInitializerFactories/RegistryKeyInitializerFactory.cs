18ae34d588893654e3f0f9f5eb32ba0b87a28e0d
Martin Márkus <martinmarkus0@gmail.com>
2019-10-23 13:06:16 +0200

    Remove commented obsolete codes

--- WindowsRegistryManager/WindowsRegistrymanager/Facades/Factories/RegistryKeyInitializerFactories/RegistryKeyInitializerFactory.cs
+++ WindowsRegistryManager/WindowsRegistrymanager/Facades/Factories/RegistryKeyInitializerFactories/RegistryKeyInitializerFactory.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Win32;
 using StrategySupporter;
-using StrategySupporter.Delegates;
 using System.Reflection;
 using WindowsRegistryManager.Attributes;
 using WindowsRegistryManager.DataObjects.WindowsRegistryAccess;
@@ -34,80 +33,5 @@ namespace WindowsRegistryManager.Facades.Factories.RegistryKeyInitializerFactori
 
             return _registryKeyInitializer.InitializeRegistryKey(windowsRegistryAccess);
         }
-
-        //public IRegistryKeyInitializer CreateInitializer(RootKey rootKey)
-        //{
-        //    IRegistryKeyInitializer initializer = null;
-        //    Type type = GetInitializerType(rootKey);
-
-        //    try
-        //    {
-        //        initializer = (IRegistryKeyInitializer)Activator.CreateInstance(type);
-        //    }
-        //    catch(Exception e) when (e is ArgumentException || e is ArgumentNullException || e is NotSupportedException
-        //        || e is TargetInvocationException || e is MethodAccessException || e is MemberAccessException
-        //        || e is InvalidComObjectException || e is COMException || e is MissingMethodException || e is TypeLoadException)
-        //    {
-        //        Console.WriteLine(e.ToString());
-        //    }
-
-        //    return initializer;
-        //}
-
-        //private Type GetInitializerType(RootKey rootKey)
-        //{
-        //    Type initializerType = null;
-        //    Type[] assemblyTypes = GetAssemblyTypes();
-
-        //    foreach (Type type in assemblyTypes)
-        //    {
-        //        RegistryAccessAttribute[] attributes = GetRegistryAccessAttributes(type);
-        //        if (attributes == null || attributes.Length <= 0) continue;
-
-        //        RegistryAccessAttribute attribute;
-        //        attribute = attributes[0];
-
-        //        if (attribute.RootKey == rootKey)
-        //        {
-        //            initializerType = type;
-        //            break;
-        //        }
-        //    }
-
-        //    return initializerType;
-        //}
-
-        //private Type[] GetAssemblyTypes()
-        //{
-        //    Type[] types = null;
-        //    Assembly assembly = Assembly.GetExecutingAssembly();
-
-        //    try
-        //    {
-        //        types = assembly.GetTypes();
-        //    }
-        //    catch (ReflectionTypeLoadException e)
-        //    {
-        //        Console.WriteLine(e.ToString());
-        //    }
-
-        //    return types;
-        //}
-
-        //private RegistryAccessAttribute[] GetRegistryAccessAttributes(Type type)
-        //{
-        //    RegistryAccessAttribute[] attributes = null;
-
-        //    try
-        //    {
-        //        attributes = (RegistryAccessAttribute[])type?.GetCustomAttributes(typeof(RegistryAccessAttribute));
-        //    }
-        //    catch(Exception e) when (e is ArgumentNullException || e is ArgumentException || e is NotSupportedException || e is TypeLoadException)
-        //    {
-        //        Console.WriteLine(e.ToString());
-        //    }
-
-        //    return attributes;
-        //}
     }
 }
--- WindowsRegistryManager/WindowsRegistrymanager/Facades/Serializers/ByteArraySerializer.cs
+++ WindowsRegistryManager/WindowsRegistrymanager/Facades/Serializers/ByteArraySerializer.cs
@@ -2,49 +2,11 @@
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Xml;
-using System.Xml.Serialization;
 
 namespace WindowsRegistryManager.Facades.Serializers
 {
     internal class ByteArraySerializer : IByteArraySerializer
     {
-        //public byte[] Serialize<U>(U objectToSerialize)
-        //{
-        //    if (objectToSerialize == null)
-        //    {
-        //        throw new ArgumentNullException();
-        //    }
-
-        //    XmlSerializer serializer = new XmlSerializer(typeof(U));
-
-        //    using (MemoryStream memoryStream = new MemoryStream())
-        //    {
-        //        using (XmlWriter xmlWriter = XmlWriter.Create(memoryStream))
-        //        {
-        //            serializer.Serialize(xmlWriter, objectToSerialize);
-        //            return memoryStream.ToArray();
-        //        }
-        //    }
-        //}
-
-        //public U Deserialize<U>(byte[] byteArray)
-        //{
-        //    if (byteArray == null || byteArray.Length == 0)
-        //    {
-        //        throw new InvalidOperationException();
-        //    }
-
-        //    XmlSerializer serializer = new XmlSerializer(typeof(U));
-
-        //    using (MemoryStream memoryStream = new MemoryStream(byteArray))
-        //    {
-        //        using (XmlReader xmlReader = XmlReader.Create(memoryStream))
-        //        {
-        //            return (U)serializer.Deserialize(xmlReader);
-        //        }
-        //    }
-        //}
 
         public byte[] Serialize<U>(U objectToSerialize)
         {
